dae6ec236f09e4bc43b99d9bb4f47bb07eedfe47
amitbadala <amitbadala07@gmail.com>
2021-01-24 11:48:35 +0530

    Add models & config for different aspects of the project

--- _middleware/validate-request.js
+++ _middleware/validate-request.js
@@ -1,16 +1,19 @@
 module.exports = validateRequest;
 
-function validateRequest(req, next, schema) {
-    const options = {
-        abortEarly: false, // include all errors
-        allowUnknown: true, // ignore unknown props
-        stripUnknown: true // remove unknown props
-    };
-    const { error, value } = schema.validate(req.body, options);
-    if (error) {
-        next(`Validation error: ${error.details.map(x => x.message).join(', ')}`);
-    } else {
-        req.body = value;
-        next();
-    }
+function validateRequest(req, res, next, schema) {
+  const options = {
+    abortEarly: false, // include all errors
+    allowUnknown: true, // ignore unknown props
+    stripUnknown: true, // remove unknown props
+  };
+  const { error, value } = schema.validate(req.body, options);
+  console.log(error, "ERROR");
+  if (error) {
+    res.send(
+      `Validation error: ${error.details.map((x) => x.message).join(", ")}`
+    );
+  } else {
+    req.body = value;
+    next();
+  }
 }
